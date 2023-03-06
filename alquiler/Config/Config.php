63768a67e3c0183c02b39664029da2451b3375ac
Themorocd <73909321+Themorocd@users.noreply.github.com>
2023-03-06 11:09:33 +0100

    Update Config.php

--- alquiler/Config/Config.php
+++ alquiler/Config/Config.php
@@ -1,8 +1,8 @@
 <?php
 const base_url = "http://localhost/alquiler/";
 const host = "localhost";
-const user = "root";
-const pass = "";
-const db = "alquiler";
+const user = "themorocd";
+const pass = "trebujena";
+const db = "9750806_alquiler";
 const charset = "charset=utf8";
 ?>
